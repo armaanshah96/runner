84599e85b462a8881b819808d21849b9e7379989
Armaan Shah <armaanshah96@gmail.com>
2016-07-01 00:08:14 +0200

    bmi function and test

--- R/bmi.R
+++ R/bmi.R
@@ -1,10 +1,11 @@
-# bmi
+#' Calculates Body Mass Index
 #' @param height Height of individual of numeric vector. In inches if english units and meters if metric units.
 #' @param weight Weight of individual of numeric vector. In pounds if english units and kilograms if metric units.
 #' @param units  Character vector representing the units convention used for height and weight. Either "english" or "metric". Default is "english".
 #' @return numeric vector representing the BMI of an individual based on their height and weight
-#' @example
+#' @examples
 #' bmi(72,1.7,"metric")
+#' bmi(66,140)
 #' @export
 bmi <- function(height,weight,units="english") {
   # Check parameter types are correct
@@ -22,6 +23,7 @@ bmi <- function(height,weight,units="english") {
   else{
     if(units != "english" && units != "metric") {
       stop("units must be either 'english' or 'metric'")
+      geterrmessage()
     }
   }
 
--- tests/testthat/test-bmi.R
+++ tests/testthat/test-bmi.R
@@ -2,12 +2,42 @@ library(runner)
 
 context("BMI Measurement")
 
-test_that("measuring BMI in english units", {
+# test english
+test_that("measuring BMI in hidden english units", {
   expect_equal(bmi(66,140), 22.59)
-
 })
 
+test_that("measuring BMI in english units", {
+  expect_equal(bmi(66,140,"english"), 22.59)
+})
+
+
+# test metric
 test_that("measuring BMI in metric units", {
   expect_equal(bmi(1.7,72,"metric"), 24.91)
 
 })
+
+# # test wrong height type
+# test_that("wrong height type is provided", {
+#   expect_equal(bmi("1",72,"metric"), "height must be a numeric!")
+#
+# })
+#
+# # test wrong weight type
+# test_that("wrong weight type is provided", {
+#   expect_equal(bmi(1.7,"72","metric"), "weight must be a numeric!")
+#
+# })
+#
+# # test wrong units type
+# test_that("wrong unitts type is provided", {
+#   expect_equal(bmi(1.7,72,11), "Error in bmi(1.7, 72, 11) : units must be a character!")
+#
+# })
+#
+# # test wrong units option selected, not metric or english
+# test_that("neither english or metric units are provided", {
+#   expect_equal(bmi(1.7,72,"SI units"), stop("units must be either 'english' or 'metric'"))
+#
+# })
